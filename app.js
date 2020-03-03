1b56d5da3e0c42a6b1b391a5b95cecc33044387e
Dustin Myers <dustinblaine.myers@gmail.com>
2020-03-03 11:34:07 -0500

    Express app up and running with es6 template engine as views engine

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const es6Renderer = require('express-es6-template-engine');
+
+var indexRouter = require('./routes/index');
+
+
+var app = express();
+
+app.engine('html', es6Renderer);
+app.set('views', 'views');
+app.set('view engine', 'html');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+
+module.exports = app;
